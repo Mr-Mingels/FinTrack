00a17e7c4ea1b48cce533b0eb4a3f3b1c0935781
Pierre <Pierre.Mingels@protonmail.com>
2023-07-03 18:30:49 +0200

    updated my folder structure and started on my authentication page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import React, { lazy, Suspense, useEffect, useState  } from "react";
 
-function App() {
+const Authenticate = lazy(() => import('./components/Authenticate'));
+
+const App = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const RedirectToHome = () => {
+    const navigate = useNavigate();
+    React.useEffect(() => {
+      navigate('/');
+    }, [navigate]);
+  }
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+  
+    window.addEventListener('resize', handleResize);
+  
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Suspense fallback={<div className="loaderWrapper"><span className="loader"></span></div>}>
+            <Routes>
+              <Route path="/sign-up" element={<Authenticate windowWidth={windowWidth}/>} />
+              <Route path="/log-in" element={<Authenticate windowWidth={windowWidth}/>} />
+              <Route path='*' element={<RedirectToHome />}/>
+            </Routes>
+        </Suspense>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Authenticate.js
@@ -0,0 +1,173 @@
+import { BrowserRouter, Routes, Route, useNavigate, useLocation, Link } from "react-router-dom";
+import React, { lazy, Suspense, useEffect, useState  } from "react";
+import axios from 'axios';
+import '../styles/Authenticate.css'
+import logo from '../assets/logo.png'
+
+const Authenticate = ({ windowWidth }) => {
+    const [authConfig, setAuthConfig] = useState(null)
+    const [redUserNamePlaceHolder, setRedUserNamePlaceHolder] = useState(false)
+    const [redEmailPlaceHolder, setRedEmailPlaceHolder] = useState(false)
+    const [redPasswordPlaceholder, setRedPasswordPlaceholder] = useState(false)
+    const [loader, setLoader] = useState(false)
+    const [email, setEmail] = useState({
+        value: '',
+        placeholder: 'Enter your email'
+      });
+    const [userName, setUserName] = useState({
+        value: '',
+        placeholder: 'Enter your username'
+      });
+    const [password, setPassword] = useState({
+        value: '',
+        placeholder: 'Enter your password'
+      });
+
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (location.pathname === '/sign-up') {
+            setAuthConfig(true)
+        } else {
+            setAuthConfig(false)
+        }
+    },[location])
+
+    const handleFormSubmit = async (event) => {
+        setLoader(true)
+        event.preventDefault();
+        if (userName.value === '') {
+            setRedUserNamePlaceHolder(true)
+            setUserName({ ...userName, placeholder: 'Please Fill Out This Field' })
+        }
+        if (email.value === '') {
+            setRedEmailPlaceHolder(true)
+            setEmail({ ...email, placeholder: 'Please Fill Out This Field' })
+        }
+        if (password.value === '') {
+            setRedPasswordPlaceholder(true)
+            setPassword({ ...password, placeholder: 'Please Fill Out This Field' })
+        }
+        if (password.value === '' || email.value === '' || userName.value === '') {
+            setLoader(false)
+            return;
+        }
+
+        const user = {
+            email: email.value.toUpperCase(),
+            username: userName.value.toUpperCase(),
+            password: password.value
+        }
+
+        try {
+            const path = authConfig ? 'sign-up-page' : 'log-in-page';
+
+            const response = await axios.post(`/${path}`, user, { withCredentials: true });
+            if (authConfig && response.status === 200) {
+                navigate('/log-in')
+                setEmail({ ...email, value: ''})
+                setPassword({ ...password, value: ''})
+                setUserName({ ...userName, value: ''})
+            } else if (!authConfig && response.status === 200) {
+                navigate('/')
+            }
+            setLoader(false)
+        } catch (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.error('Error message: ', error.response.data.message);
+                if (error.response.data.message === 'Email and Username are incorrect') {
+                    setRedEmailPlaceHolder(true)
+                    setEmail({ ...email, value: '', placeholder: 'Incorrect email' })
+                    setRedUserNamePlaceHolder(true)
+                    setUserName({ ...userName, value: '', placeholder: 'Incorrect username' })
+                } else if (error.response.data.message === 'Email is incorrect') {
+                    setRedEmailPlaceHolder(true)
+                    setEmail({ ...email, value: '', placeholder: 'Incorrect email' })
+                } else if (error.response.data.message === 'Username is incorrect' || 
+                    error.response.data.message === "Email and username do not match the same user") {
+                    setRedUserNamePlaceHolder(true)
+                    setUserName({ ...userName, value: '', placeholder: 'Incorrect username' })
+                } else if (error.response.data.message === 'Incorrect password') {
+                    setRedPasswordPlaceholder(true)
+                    setPassword({ ...password, value: '', placeholder: 'Incorrect password' })
+                } else if (error.response.data.message === "Email has already been taken") {
+                    setRedEmailPlaceHolder(true)
+                    setEmail({ ...email, value: '', placeholder: 'Email has already been taken' })
+                } else if (error.response.data.message === "Username has already been taken") {
+                    setRedUserNamePlaceHolder(true)
+                    setUserName({ ...userName, value: '', placeholder: 'Username has already been taken' })
+                } else if (error.response.data.message === "Email and Username have already been taken") {
+                    setRedEmailPlaceHolder(true)
+                    setEmail({ ...email, value: '', placeholder: 'Email has already been taken' })
+                    setRedUserNamePlaceHolder(true)
+                    setUserName({ ...userName, value: '', placeholder: 'Username has already been taken' })
+                }
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.error('Error', error.message);
+            }
+            setLoader(false)
+        }
+    }
+    
+      useEffect(() => {
+        if(userName.value !== '') {
+            setRedUserNamePlaceHolder(false)
+            setUserName({ ...userName, placeholder: 'Enter your username' })
+        }
+      },[userName.value])
+      
+      useEffect(() => {
+        if (email.value !== '') {
+            setRedEmailPlaceHolder(false)
+            setEmail({ ...email, placeholder: 'Enter your email' })
+        }
+      }, [email.value])
+    
+      useEffect(() => {
+        if (password.value !== '') {
+            setRedPasswordPlaceholder(false)
+            setPassword({ ...password, placeholder: 'Enter your password' })
+        }
+      }, [password.value])
+
+    return (
+        <section className="authWrapper">
+            <div className="authContentWrapper">
+                <h2 className="authAppTitle"><img className="authLogoImg" src={logo} />finTrack</h2>
+                    <div className="authFormModalWrapper">
+                        <h1 className="authCreateAccountTitle">{authConfig ? 'Create your account' : 'Log into your account'}</h1>
+                        <div className="authFormWrapper">
+                            <form method="POST" className="authForm" onSubmit={handleFormSubmit}>
+                                <label className="authLabel">Email<span className="authRequireTag">*</span></label>
+                                <input type="email" name="email" placeholder={email.placeholder} 
+                                className={`authFormEmailInput ${redEmailPlaceHolder ? 'field' : ''}`} 
+                                onChange={(e) => setEmail({ ...email, value: e.target.value })} value={email.value}/>
+                                <label className="authLabel">UserName<span className="authRequireTag">*</span></label>
+                                <input maxLength='16' name="username" placeholder={userName.placeholder} 
+                                className={`authFormUserNameInput ${redUserNamePlaceHolder ? 'field' : ''}`}
+                                onChange={(e) => setUserName({ ...userName, value: e.target.value })} value={userName.value}/>
+                                <label className="authLabel">Password<span className="authRequireTag">*</span></label>
+                                <input type="password" name="password" placeholder={password.placeholder} 
+                                className={`authFormPassWordInput ${redPasswordPlaceholder ? 'field' : ''}`} 
+                                onChange={(e) => setPassword({ ...password, value: e.target.value })} value={password.value}/>
+                                {loader ? (
+                                    <button className="authConfirmBtn authLoader"><span class="modalLoader"></span></button>
+                                ) : (
+                                    <button type="submit" className="authConfirmBtn">{authConfig ? 'Sign Up' : 'Log In'}</button>
+                                )}  
+                            </form>
+                            <small className="authFormQuestion">{authConfig ? 'Already have an account?' : `Don't have an account?`} <Link 
+                            className="authFormRedirectLink" to={authConfig ? '/log-in' : `/sign-up`} onMouseDown={(e) => e.preventDefault()}>
+                            {authConfig ? 'Log in' : `Sign up`}</Link></small>
+                        </div>
+                    </div>
+                </div>
+        </section>
+    )
+}
+
+export default Authenticate
